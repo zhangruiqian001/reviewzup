dbc390399c3d25f543521f98f2c2be6a86c47c80
zhongjia <zhongjia_wei@163.com>
2016-09-01 10:31:03 +0800

    首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
+<div class="jumbotron rz_mask">
+    <div class="container" style="text-align: center;">
+        <h1 style="color: white;font-size: 4rem;margin-top: 60px;">BEST HEADLINE IN THE WORLD</h1>
+        <p style="color: white;">We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.</p>
+        <p><button type="button" class="btn btn-primary btn-lg">Large button</button></p>
     </div>
 </div>
+<section class="container content-section">
+    <div class="row">
+        <div class="col-lg-6">
+            <h2 class="section-pad">HOW IT WORKS</h2>
+            <p style="padding-right: 40px;">
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+            </p>
+            <p><button type="button" class="btn btn-primary">LEARN MORE</button></p>
+        </div>
+        <div class="col-lg-3">
+            <h2 class="section-pad">NEW</h2>
+            <img src="/images/home/1-media.jpg" class="circle-image">
+        </div>
+        <div class="col-lg-3">
+            <h2 class="section-pad">OLD</h2>
+            <img src="/images/home/1-media.jpg" class="circle-image">
+        </div>
+    </div>
+</section>
+<section class="container content-section">
+    <div class="row">
+        <div class="col-lg-6">
+            <h2 class="section-pad">HOW IT WORKS</h2>
+            <p style="padding-right: 40px;">
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+                We are visual creatures. Our system automatically pulls featured images and embeds them into each tweet. No fuss. Just beautiful media in each and every tweet.
+            </p>
+            <p><button type="button" class="btn btn-primary">LEARN MORE</button></p>
+        </div>
+        <div class="col-lg-3">
+            <h2 class="section-pad">NEW</h2>
+            <img src="/images/home/1-media.jpg" class="circle-image">
+        </div>
+        <div class="col-lg-3">
+            <h2 class="section-pad">OLD</h2>
+            <img src="/images/home/1-media.jpg" class="circle-image">
+        </div>
+    </div>
+</section>
 @endsection
