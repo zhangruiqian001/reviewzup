4d513965a333faf2dce918d3e12b5f0fecb4448b
zhangruiqian <zhangrq@cdmhzx.com>
2016-09-09 00:20:29 +0800

    注册页面改造

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,108 +3,66 @@
 @section('content')
     <div class="container">
         <div class="row">
-            {{--</div>--}}
-            {{--<div class="panel panel-default" style="margin: auto;width: 30%">--}}
-            {{--<div class="panel-heading" style="margin: auto;text-align: center">Sign UP</div>--}}
-            {{--<div class="panel-body">--}}
-            {{--<form>--}}
-            {{--<div class="input-group">--}}
-            {{--<span class="input-group-addon" id="basic-addon1">--}}
-            {{--<span class="glyphicon glyphicon-user"></span>--}}
-            {{--</span>--}}
-            {{--<input type="text" id="name" class="form-control" placeholder="Name"--}}
-            {{--aria-describedby="basic-addon1">--}}
-            {{--</div>--}}
-            {{--<br>--}}
-            {{--<div class="input-group">--}}
-            {{--<span class="input-group-addon" id="basic-addon1">--}}
-            {{--<span class="glyphicon glyphicon-envelope"></span>--}}
-            {{--</span>--}}
-            {{--<input type="text" id="email" class="form-control" placeholder="Email"--}}
-            {{--aria-describedby="basic-addon1">--}}
-            {{--</div>--}}
-            {{--<br>--}}
-            {{--<div class="input-group">--}}
-            {{--<span class="input-group-addon" id="basic-addon1">--}}
-            {{--<span class="glyphicon glyphicon-asterisk"></span>--}}
-            {{--</span>--}}
-            {{--<input type="password" id="password" class="form-control" placeholder="Password"--}}
-            {{--aria-describedby="basic-addon1">--}}
-            {{--</div>--}}
-            {{--<br>--}}
-            {{--<div class="input-group">--}}
-            {{--<input type="checkbox">&nbsp; <span style="font-size: 0.5rem">I agree to bound by Reviewzup's <a href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></span>--}}
-            {{--</div>--}}
-            {{--<div class="btn-group">--}}
-            {{--<button type="submit" class="btn btn-primary">--}}
-            {{--Login--}}
-            {{--</button>--}}
-            {{--</div>--}}
-            {{--</form>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
-                    <div class="panel panel-default">
-                        <div style="text-align: center;margin-top: 3rem">
-                            Login
-                        </div>
-                        <div class="panel-body">
-                            <form class="col-md-10 col-md-offset-1 form-horizontal" role="form" method="POST"
-                                  action="{{ url('/login') }}">
-                                {{ csrf_field() }}
-                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} input-group">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div style="text-align: center;margin-top: 3rem;font-size: 2rem">
+                        Login
+                    </div>
+                    <div class="panel-body">
+                        <form class="col-md-10 col-md-offset-1 form-horizontal" role="form" method="POST"
+                              action="{{ url('/login') }}">
+                            {{ csrf_field() }}
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} input-group">
                                     <span class="input-group-addon" id="basic-addon1">
                                         <span class="glyphicon glyphicon-envelope"></span>
                                     </span>
-                                    <input type="text" id="email" class="form-control" placeholder="Email" name="email"
-                                           value="{{ old('email') }}">
-                                </div>
-                                <div class="input-group row form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <input type="text" id="email" class="form-control" placeholder="Email" name="email"
+                                       value="{{ old('email') }}">
+                            </div>
+                            <div class="input-group row form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                     <span class="input-group-addon" id="basic-addon1">
                                         <span class="glyphicon glyphicon-asterisk"></span>
                                     </span>
-                                    <input type="password" id="password" class="form-control" placeholder="Password"
-                                           name="password">
-                                </div>
-                                @if ($errors->has('email'))
-                                    <div class="has-error ">
+                                <input type="password" id="password" class="form-control" placeholder="Password"
+                                       name="password">
+                            </div>
+                            @if ($errors->has('email'))
+                                <div class="has-error ">
                                         <span class="help-block">
                                             <strong>*{{ $errors->first('email') }}</strong>
                                         </span>
-                                    </div>
-                                @endif
-                                @if ($errors->has('password'))
-                                    <div class="has-error ">
+                                </div>
+                            @endif
+                            @if ($errors->has('password'))
+                                <div class="has-error ">
                                         <span class="help-block">
                                             <strong>*{{ $errors->first('password') }}</strong>
                                         </span>
-                                    </div>
-                                @endif
-                                <br>
-                                <div class="row">
-                                    <div class="col-md-6" style="padding: 0">
-                                        <label>
-                                            <input type="checkbox" name="remember"> <span style="font-size: 0.8rem">Remember Me</span>
-                                        </label>
-                                    </div>
-                                    <div class="col-md-6" style="padding: 0">
-                                        <a href="{{ url('/password/reset') }}"
-                                           style="font-size: 0.8rem">Forgot Password?</a>
-                                    </div>
                                 </div>
-                                <br>
-                                <div class="row" style="margin-bottom: 3rem">
-                                    <button type="submit" class="btn btn-primary col-md-12"
-                                            style="padding:0;height: 3rem">
-                                        Login
-                                    </button>
+                            @endif
+                            <br>
+                            <div class="row">
+                                <div class="col-md-6" style="padding: 0">
+                                    <label>
+                                        <input type="checkbox" name="remember"> <span style="font-size: 0.8rem">Remember Me</span>
+                                    </label>
                                 </div>
-                            </form>
-                        </div>
+                                <div class="col-md-6" style="padding: 0">
+                                    <a href="{{ url('/password/reset') }}"
+                                       style="font-size: 0.8rem">Forgot Password?</a>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row" style="margin-bottom: 3rem">
+                                <button type="submit" class="btn btn-primary"
+                                        style="padding:0;height: 3rem;width: 100%">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
                     </div>
-
                 </div>
             </div>
         </div>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,86 +3,6 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Register</div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                            {{ csrf_field() }}
-
-                            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <label for="name" class="col-md-4 control-label">Name</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control" name="name"
-                                           value="{{ old('name') }}">
-
-                                    @if ($errors->has('name'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email"
-                                           value="{{ old('email') }}">
-
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password">
-
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                           name="password_confirmation">
-
-                                    @if ($errors->has('password_confirmation'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-user"></i> Register
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div style="text-align: center; margin-top: 3rem;font-size: 2rem">
@@ -92,39 +12,74 @@
                         <form class="col-md-10 col-md-offset-1 form-horizontal" role="form" method="POST"
                               action="{{ url('/register') }}">
                             {{ csrf_field() }}
-                            <div class="input-group form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <span class="input-group-addon" id="basic-addon1">
-                                    <span class="glyphicon glyphicon-user"></span>
-                                </span>
-                                <input type="text" id="name" class="form-control" placeholder="Name" name="name"
-                                       aria-describedby="basic-addon1">
+                            <div class="form-group {{ $errors->has('name') ? ' has-error' : '' }}">
+                                <div class="input-group ">
+                                    <span class="input-group-addon" id="basic-addon1">
+                                        <span class="glyphicon glyphicon-user"></span>
+                                    </span>
+                                    <input type="text" id="name" class="form-control" placeholder="Name" name="name"
+                                           aria-describedby="basic-addon1" value="{{old('name')}}">
+                                </div>
+                                @if ($errors->has('name'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                @endif
                             </div>
-                            <div class="input-group form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <span class="input-group-addon" id="basic-addon1">
-                                    <span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input type="text" id="email" class="form-control" placeholder="Email" name="email"
-                                       aria-describedby="basic-addon1">
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <div class="input-group ">
+                                    <span class="input-group-addon" id="basic-addon1">
+                                        <span class="glyphicon glyphicon-envelope"></span>
+                                    </span>
+                                    <input type="text" id="email" class="form-control" placeholder="Email" name="email"
+                                           aria-describedby="basic-addon1" value="{{old('email')}}">
+                                </div>
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
                             </div>
-                            <div class="input-group form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <span class="input-group-addon" id="basic-addon1">
-                                    <span class="glyphicon glyphicon-asterisk"></span>
-                                </span>
-                                <input type="password" id="password" class="form-control" placeholder="Password" name="password"
-                                       aria-describedby="basic-addon1">
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="basic-addon1">
+                                        <span class="glyphicon glyphicon-asterisk"></span>
+                                    </span>
+                                    <input type="password" id="password" class="form-control" placeholder="Password"
+                                           name="password"
+                                           aria-describedby="basic-addon1">
+                                </div>
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
                             </div>
-                            <div class="input-group form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                                <span class="input-group-addon" id="basic-addon1">
-                                    <span class="glyphicon glyphicon-asterisk"></span>
-                                </span>
-                                <input type="password" id="password-confirm" class="form-control"
-                                       placeholder="Confirm your Password" name="password_confirmation"
-                                       aria-describedby="basic-addon1">
+                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="basic-addon1">
+                                        <span class="glyphicon glyphicon-asterisk"></span>
+                                    </span>
+                                    <input type="password" id="password-confirm" class="form-control"
+                                           placeholder="Confirm your Password" name="password_confirmation"
+                                           aria-describedby="basic-addon1">
+                                </div>
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
                             </div>
-
-                            <div class="input-group form-group">
-                                <input type="checkbox">&nbsp; <span style="font-size: 0.5rem">I agree to bound by Reviewzup's <a
-                                    href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></span>
+                            <div class="form-group{{ $errors->has('agreement') ? ' has-error' : '' }}">
+                                <div class="input-group">
+                                    <input type="checkbox" id="agreement" name="agreement">&nbsp; <span style="font-size: 0.5rem">I agree to bound by Reviewzup's <a
+                                                href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></span>
+                                </div>
+                                @if ($errors->has('agreement'))
+                                    <span class="help-block">
+                                            <strong>{{ $errors->first('agreement') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary col-md-12"
@@ -143,50 +98,4 @@
             </div>
         </div>
     </div>
-
-
-
-
-    {{--<div class="panel panel-default" style="margin: auto;width: 30%">--}}
-    {{--<div class="panel-heading" style="margin: auto;text-align: center">Sign UP</div>--}}
-    {{--<div class="panel-body">--}}
-    {{--<form>--}}
-    {{--<div class="input-group">--}}
-    {{--<span class="input-group-addon" id="basic-addon1">--}}
-    {{--<span class="glyphicon glyphicon-user"></span>--}}
-    {{--</span>--}}
-    {{--<input type="text" id="name" class="form-control" placeholder="Name"--}}
-    {{--aria-describedby="basic-addon1">--}}
-    {{--</div>--}}
-    {{--<br>--}}
-    {{--<div class="input-group">--}}
-    {{--<span class="input-group-addon" id="basic-addon1">--}}
-    {{--<span class="glyphicon glyphicon-envelope"></span>--}}
-    {{--</span>--}}
-    {{--<input type="text" id="email" class="form-control" placeholder="Email"--}}
-    {{--aria-describedby="basic-addon1">--}}
-    {{--</div>--}}
-    {{--<br>--}}
-    {{--<div class="input-group">--}}
-    {{--<span class="input-group-addon" id="basic-addon1">--}}
-    {{--<span class="glyphicon glyphicon-asterisk"></span>--}}
-    {{--</span>--}}
-    {{--<input type="password" id="password" class="form-control" placeholder="Password"--}}
-    {{--aria-describedby="basic-addon1">--}}
-    {{--</div>--}}
-    {{--<br>--}}
-    {{--<div class="input-group">--}}
-    {{--<input type="checkbox">&nbsp; <span style="font-size: 0.5rem">I agree to bound by Reviewzup's <a--}}
-    {{--href="#">Terms of Service</a> and <a href="#">Privacy Policy.</a></span>--}}
-    {{--</div>--}}
-    {{--<div class="btn-group">--}}
-    {{--<button type="submit" class="btn btn-primary">--}}
-    {{--Login--}}
-    {{--</button>--}}
-    {{--</div>--}}
-    {{--</form>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-
-    {{--</div>--}}
 @endsection
